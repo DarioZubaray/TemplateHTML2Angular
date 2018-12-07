5d9f50233421c07fa8477a0741784c276b23d3fb
Dario Zubaray <dzubaray@gmail.com>
2018-12-07 09:14:40 -0300

    implementando la busuqeda de items

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   producto: ProductoInterface[] = [];
+  productosFiltrados: ProductoInterface[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
@@ -19,14 +20,44 @@ export class ProductosService {
 
   private cargarProductos() {
     const urlFirebase = 'https://angular-templatehtml.firebaseio.com/productos_idx.json';
-    this.http.get(urlFirebase)
-        .subscribe( (resp: ProductoInterface[]) => {
-          console.log(resp);
-          this.producto = resp;
-        });
+
+    return new Promise( (resolve, reject) => {
+      this.http.get(urlFirebase)
+          .subscribe( (resp: ProductoInterface[]) => {
+            console.log(resp);
+            this.producto = resp;
+            resolve();
+          });
+    });
   }
 
   public getProducto(id: string) {
     return this.http.get(`https://angular-templatehtml.firebaseio.com/productos/${id}.json`);
+  }
+
+  public buscarProducto(termino: string) {
+    if (this.producto.length === 0) {
+      this.cargarProductos().then( () => {
+        this.filtrarProductos(termino);
+      });
+
+    } else {
+      this.filtrarProductos(termino);
+    }
+    console.log(this.productosFiltrados);
+  }
+
+  private filtrarProductos(termino: string) {
+    this.productosFiltrados = [];
+    termino = termino.toLowerCase();
+    this.producto.forEach( p => {
+      const titulo = p.titulo.toLowerCase();
+      const categoria = p.categoria.toLowerCase();
+
+      if (categoria.indexOf(termino) >= 0
+          || titulo.indexOf(termino) >= 0) {
+        this.productosFiltrados.push(p);
+      }
+    });
   }
 }
